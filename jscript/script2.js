134e6582b79eb63b1cb55247da558d7f3827198a
Mariana Ito <marianaitopinto@gmail.com>
2022-03-04 19:30:34 -0300

    pegando as infos da API

--- jscript/script2.js
+++ jscript/script2.js
@@ -1,7 +1,6 @@
-let x = document.getElementById("demo");
-let lat;
-let lon;
-const URL_SECOND_API = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={4e82254cee731f87bb90ca60059ca134}`
+let lat = 0;
+let lon = 0;
+let URL_SECOND_API = '';
 
 function localizaoAutomatica() {
     
@@ -14,17 +13,22 @@ function localizaoAutomatica() {
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
   
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
+    lat = (position.coords.latitude).toFixed(2);
+    lon = (position.coords.longitude).toFixed(2);
     console.log(lat);
     console.log(lon);
+    URL_SECOND_API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4e82254cee731f87bb90ca60059ca134`
+    searchByPosition();
 }
 
 function searchByPosition() {
     const promisse = axios.get(URL_SECOND_API);
     promisse.then(renderWeather);
     promisse.catch()
+}
+
+function renderWeather(data) {
+  console.log(data);
+  
 }
